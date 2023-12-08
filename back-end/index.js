7dd46a4e5421b04bba215a06fe3eae90f6499c57
jockaplay <jocksonmatheus@gmail.com>
2023-12-08 14:26:12 -0300

    Connect database and validate any data

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,46 @@
-const express = require('express')
+require('dotenv').config();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const express = require('express');
+
+const User = require('./models/User');
+
 const app = express();
+app.use(express.json())
 const PORT = 3030;
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.status(200).json({msg: 'Hello World'})
 })
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+app.post('/auth/register', async(req, res) => {
+    const {name, email, pass, confirmpass} = req.body
+    if (!name) {
+        return res.status(422).json({msg: "Name is required."})
+    }
+    if (!pass) {
+        return res.status(422).json({msg: "Password is required."})
+    } else if (pass != confirmpass){
+        return res.status(422).json({msg: "Passwords not match."})
+    }
+    if (!email) {
+        return res.status(422).json({msg: "Email is required."})
+    }
+
+    const userExists = await User.findOne({ email: email})
+
+    if (userExists) { return res.status(422).json({msg: "This Email already be registered."}) }
+
+    return res.status(200).json({msg: "User registred."})
 })
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.8xjfylx.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`)
+    })
+    console.log("Database conected.")
+}).catch((err) => console.log(err))
